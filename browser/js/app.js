8acc31ab1401da651eadfeea13603eeb10a7bff6
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-05-01 12:26:08 +0100

    04-routing

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,1 +1,10 @@
 var App = angular.module('ToodooApp', []);
+
+App.config(function($locationProvider, $routeProvider) {
+  $locationProvider.html5Mode(true);
+
+  $routeProvider.
+    when('/', {
+      templateUrl: '/partials/index.html'
+    });
+});
