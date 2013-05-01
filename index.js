c3f28e8b8626319a31939cbf285e4f8e8e7f9336
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-05-01 12:29:07 +0100

    06-safe-reload

--- index.js
+++ index.js
@@ -14,9 +14,13 @@ var file = new (static.Server)(__dirname + '/browser');
 server.on('request', function(req, res) {
   file.serve(req, res, function(err) {
     if (err) {
-      console.error('error processing request %s %s:', req.method, req.url, err);
-      res.writeHead(err.status);
-      res.end(err.message);
+      if (err.status == 404 && ~req.headers.accept.split(',').indexOf('text/html')) {
+        file.serveFile('/index.html', 200, {}, req, res);
+      } else {
+        console.error('error processing request %s %s:', req.method, req.url, err);
+        res.writeHead(err.status);
+        res.end(err.message);
+      }
     }
   });
 });
